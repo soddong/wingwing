c0cc3390a4590eaf9c8d9e7ee56605d73a7d06d3
전정민 <junsk50@naver.com>
2024-10-29 17:44:38 +0900

    Init: 메인 서버 초기 설정 파일 및 젠킨스 파일

---
+++ shieldron-main-server/src/test/java/com/ssafy/shieldron/ShieldronApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.shieldron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShieldronApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
