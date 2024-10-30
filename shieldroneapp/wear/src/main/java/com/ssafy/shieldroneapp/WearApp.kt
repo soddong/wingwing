cf0e11de6dcb4264fdf9434c08d022b884a629ed
HannaKang <hanna0175@gmail.com>
2024-10-31 02:49:34 +0900

    [#S11P31A307-186] Feat: 메인 화면 UI 개선
    - AlertScreen 추가 및 sensorViewModel의 currentScreen 상태에 따라 화면 전환 기능 구현

--- shieldroneapp/wear/src/main/java/com/ssafy/shieldroneapp/WearApp.kt
+++ shieldroneapp/wear/src/main/java/com/ssafy/shieldroneapp/WearApp.kt
@@ -1,29 +1,49 @@
 package com.ssafy.shieldroneapp
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.wear.tooling.preview.devices.WearDevices
 import com.ssafy.shieldroneapp.ui.components.Layout
+import com.ssafy.shieldroneapp.ui.screens.AlertScreen
 import com.ssafy.shieldroneapp.ui.screens.MainScreen
 import com.ssafy.shieldroneapp.ui.theme.ShieldroneappTheme
 import com.ssafy.shieldroneapp.viewmodels.SensorViewModel
-import androidx.compose.runtime.remember
 
 @Composable
 fun WearApp() {
-    val sensorViewModel = remember { SensorViewModel() }  
-    
+    val sensorViewModel = remember { SensorViewModel() }
+
     ShieldroneappTheme {
         Layout(
             children = {
-                MainScreen(sensorViewModel = sensorViewModel)
+                when (sensorViewModel.currentScreen) {
+                    is SensorViewModel.Screen.Main -> MainScreen(
+                        sensorViewModel = sensorViewModel
+                    )
+                    is SensorViewModel.Screen.Alert -> AlertScreen(
+                        onSafeConfirm = { sensorViewModel.hideAlert() },
+                    )
+                }
             }
         )
     }
 }
 
+//@Preview(device = WearDevices.LARGE_ROUND, showSystemUi = true)
+//@Composable
+//fun DefaultPreview() {
+//    WearApp()
+//}
+
 @Preview(device = WearDevices.LARGE_ROUND, showSystemUi = true)
 @Composable
-fun DefaultPreview() {
-    WearApp()
+fun AlertScreenPreview() {
+    ShieldroneappTheme {
+        Layout {
+            AlertScreen(
+                onSafeConfirm = {},
+            )
+        }
+    }
 }
