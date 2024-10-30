ca496cd1ac5c67e2c0e1fbaf5e0337f67c39c248
HannaKang <hanna0175@gmail.com>
2024-10-31 02:13:13 +0900

    [#S11P31A307-186] Feat: 알림 타이머 기능 추가

--- shieldroneapp/wear/src/main/java/com/ssafy/shieldroneapp/ui/screens/AlertScreen.kt
+++ shieldroneapp/wear/src/main/java/com/ssafy/shieldroneapp/ui/screens/AlertScreen.kt
@@ -9,12 +9,39 @@ import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
 import com.ssafy.shieldroneapp.ui.components.PrimaryButton
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.LaunchedEffect
+import kotlinx.coroutines.delay
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.platform.LocalContext
+import com.ssafy.shieldroneapp.R
 
 @Composable
 fun AlertScreen(
     onSafeConfirm: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    var timeLeft by remember { mutableStateOf(5) }
+    var isTimerRunning by remember { mutableStateOf(true) }
+
+    LaunchedEffect(isTimerRunning) {
+        while (isTimerRunning && timeLeft > 0) {
+            delay(1000L)
+            timeLeft--
+        }
+        if (timeLeft == 0) {
+            onSafeConfirm()
+        }
+    }
+
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -37,11 +64,20 @@ fun AlertScreen(
             textAlign = TextAlign.Center
         )
 
+        Text(
+            text = "버튼이 눌리지 않으면 \n ${timeLeft}초 후 자동으로 긴급 연락됩니다.",
+            style = MaterialTheme.typography.caption3,
+            textAlign = TextAlign.Center
+        )
+
         Spacer(modifier = Modifier.height(16.dp))
 
         PrimaryButton(
             text = "안전 확인",
-            onClick = onSafeConfirm
+            onClick = {
+                isTimerRunning = false
+                onSafeConfirm()
+            }
         )
     }
 }
