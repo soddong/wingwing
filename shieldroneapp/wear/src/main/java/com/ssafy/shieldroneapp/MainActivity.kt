e34783d58a948f28e4a2cbc1b6a9b75698d1b450
HannaKang <hanna0175@gmail.com>
2024-11-01 01:08:53 +0900

    [#S11P31A307-187] Feat: sensorRepository 주입 및 onStart 메서드 추가

--- shieldroneapp/wear/src/main/java/com/ssafy/shieldroneapp/MainActivity.kt
+++ shieldroneapp/wear/src/main/java/com/ssafy/shieldroneapp/MainActivity.kt
@@ -6,15 +6,24 @@ import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
 
         super.onCreate(savedInstanceState)
 
+        val sensorRepository = (application as MainApplication).sensorRepository
+
         setTheme(android.R.style.Theme_DeviceDefault)
 
         setContent {
-            WearApp()
+            WearApp(
+                sensorRepository = sensorRepository
+            )
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
     }
 }
