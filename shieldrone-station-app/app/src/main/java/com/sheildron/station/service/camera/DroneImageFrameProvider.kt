a8339330bfad3f8c0deed01d22e5afb42e601d53
soddong <aledjdygus9300@gmail.com>
2024-10-31 14:03:23 +0900

    [#S11P31A307-130] Refactor: 드론모드와 카메라모드 분리
    * 디렉토리 구조 변경
    * 드론SDK로부터 전달받는 타입과 동일하게 카메라 모드 변경

---
+++ shieldrone-station-app/app/src/main/java/com/sheildron/station/service/camera/DroneImageFrameProvider.kt
@@ -0,0 +1,13 @@
+package com.sheildron.station.src.service.camera
+
+import android.content.Context
+import android.graphics.Bitmap
+
+// TODO: DJI SDK 연결 후, 작업 필요
+class DroneImageFrameProvider(private val context: Context) : ImageFrameProvider {
+    override fun startStream(callback: (Bitmap) -> Unit) {
+    }
+
+    override fun stopStream() {
+    }
+}
