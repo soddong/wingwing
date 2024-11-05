b99bde3c31cd19074a843acc2c3cbfd5eaba7f7d
godeeyeon <godeeyeon@gmail.com>
2024-11-05 13:05:13 +0900

    [S11P31A307-238] Feat: 모바일 전체 레이아웃 생성 및 라이트/다크 모드 적용 임시 토글

--- shieldroneapp/mobile/src/main/java/com/ssafy/shieldroneapp/ui/components/Layout.kt
+++ shieldroneapp/mobile/src/main/java/com/ssafy/shieldroneapp/ui/components/Layout.kt
@@ -8,3 +8,61 @@ package com.ssafy.shieldroneapp.ui.components
  *
  * @param content 레이아웃 내에 배치할 컴포저블 콘텐츠
  */
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.ssafy.shieldroneapp.ui.theme.ShieldroneappTheme
+
+@Composable
+fun Layout (
+    content: @Composable () -> Unit
+) {
+    // 테마 전환 상태 관리
+//    var isDarkTheme by remember { mutableStateOf(true) }
+    ShieldroneappTheme {
+//    ShieldroneappTheme(darkTheme = isDarkTheme) {
+        // 전체 배경을 설정하는 Box
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colors.background)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                // 테마 전환 토글 버튼 -> 추후 다른 데로 이동
+//                Row(
+//                    modifier = Modifier
+//                        .fillMaxWidth()
+//                        .background(MaterialTheme.colors.secondary),
+//                    verticalAlignment = Alignment.CenterVertically,
+//                    horizontalArrangement = Arrangement.End
+//                ) {
+//                    Text(
+//                        text = if (isDarkTheme) "다크 모드" else "라이트 모드",
+//                        style = MaterialTheme.typography.body1,
+//                        color = MaterialTheme.colors.onSecondary
+//                    )
+//                    Switch(
+//                        checked = isDarkTheme,
+//                        onCheckedChange = { isDarkTheme = it },
+//                        colors = SwitchDefaults.colors(
+//                            checkedThumbColor = MaterialTheme.colors.onSecondary,
+//                            checkedTrackColor = MaterialTheme.colors.onSecondary,
+//                            uncheckedThumbColor = MaterialTheme.colors.onSecondary,
+//                            uncheckedTrackColor = MaterialTheme.colors.onSecondary
+//                        )
+//                    )
+//                }
+
+                // 메인 콘텐츠 영역
+                content()
+            }
+        }
+    }
+}
