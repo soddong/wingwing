22969a1d31ff50e694c494f2ef3d922c45689eb1
HannaKang <hanna0175@gmail.com>
2024-11-05 13:51:18 +0900

    [#S11P31A307-242] Feat: WebSocket 오류 처리 핸들러 파일 생성
    - WebSocket 연결, 메시지 처리, 이벤트 오류에 대한 처리 메서드 구현

--- shieldroneapp/mobile/src/main/java/com/ssafy/shieldroneapp/data/source/remote/WebSocketErrorHandler.kt
+++ shieldroneapp/mobile/src/main/java/com/ssafy/shieldroneapp/data/source/remote/WebSocketErrorHandler.kt
@@ -1,15 +1,25 @@
 package com.ssafy.shieldroneapp.data.source.remote
 
-/**
- * WebSocket 에러를 처리하는 클래스.
- *
- * WebSocket 통신 중 발생하는 오류를 관리
- *
- * 주요 메서드
- * - handleConnectionError(): 연결 관련 오류 처리
- * - handleMessageError(): 메시지 처리 중 발생한 오류 처리
- * - handleErrorEvent(): WebSocket 오류 이벤트 처리
- *
- * 이 클래스는 WebSocketService에 의해 import됩니다.
- * 또한, WebSocketConnectionManager와 협력하여 재연결 및 오류 처리를 수행합니다.
- */
+import android.content.Context
+import android.util.Log
+import com.ssafy.shieldroneapp.R
+
+class WebSocketErrorHandler(private val context: Context) {
+  // 연결 관련 오류 처리
+    fun handleConnectionError(e: Throwable) {
+        val errorMessage = context.getString(R.string.connection_error, e.message)
+        Log.e("소켓 연결 오류 발생", errorMessage)
+    }
+
+    // 메시지 처리 중 발생한 오류 처리
+    fun handleMessageError(e: Throwable) {
+        val errorMessage = context.getString(R.string.message_processing_error, e.message)
+        Log.e("소켓 메시지 처리 중 에러 발생", errorMessage)
+    }
+
+    // WebSocket 오류 이벤트 처리
+    fun handleErrorEvent(message: String) {
+        val errorMessage = context.getString(R.string.error_event, message)
+        Log.e("WebSocket 이벤트 오류 발생", errorMessage)
+    }
+}
