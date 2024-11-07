e481ecf77f6555ad4634fc1c51ff556617915107
전정민 <junsk50@naver.com>
2024-11-07 15:36:18 +0900

    [S11P31A307-262] Fix: 드론 매칭 기능 유효성 검증 추가

--- shieldrone-main-server/src/main/java/com/ssafy/shieldron/controller/DroneController.java
+++ shieldrone-main-server/src/main/java/com/ssafy/shieldron/controller/DroneController.java
@@ -7,6 +7,7 @@ import com.ssafy.shieldron.dto.response.DroneAssignmentResponse;
 import com.ssafy.shieldron.dto.response.DroneMatchResponse;
 import com.ssafy.shieldron.global.CurrentUser;
 import com.ssafy.shieldron.service.DroneService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,23 +22,22 @@ public class DroneController {
 
     private final DroneService droneService;
 
-    // TODO 유효성 검증
     @PostMapping("/routes")
-    public ResponseEntity<?> droneAssignment(@RequestBody DroneAssignmentRequest droneAssignmentRequest,
+    public ResponseEntity<?> droneAssignment(@Valid @RequestBody DroneAssignmentRequest droneAssignmentRequest,
                                              @CurrentUser String phoneNumber) {
         DroneAssignmentResponse droneAssignmentResponse = droneService.droneAssignment(droneAssignmentRequest, phoneNumber);
         return ResponseEntity.ok().body(droneAssignmentResponse);
     }
 
     @PostMapping("/cancel")
-    public ResponseEntity<?> droneCancel(@RequestBody DroneCancelRequest droneCancelRequest,
+    public ResponseEntity<?> droneCancel(@Valid @RequestBody DroneCancelRequest droneCancelRequest,
                                          @CurrentUser String phoneNumber) {
         droneService.droneCancel(droneCancelRequest, phoneNumber);
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/match")
-    public ResponseEntity<?> droneMatch(@RequestBody DroneMatchRequest droneMatchRequest,
+    public ResponseEntity<?> droneMatch(@Valid @RequestBody DroneMatchRequest droneMatchRequest,
                                         @CurrentUser String phoneNumber) {
         DroneMatchResponse droneMatchResponse = droneService.droneMatch(droneMatchRequest, phoneNumber);
         return ResponseEntity.ok().body(droneMatchResponse);
--- shieldrone-main-server/src/main/java/com/ssafy/shieldron/dto/request/DroneMatchRequest.java
+++ shieldrone-main-server/src/main/java/com/ssafy/shieldron/dto/request/DroneMatchRequest.java
@@ -1,6 +1,9 @@
 package com.ssafy.shieldron.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+
 public record DroneMatchRequest(
+        @NotNull(message = "드론 ID는 필수입니다.")
         Integer droneId
 ) {
 }
