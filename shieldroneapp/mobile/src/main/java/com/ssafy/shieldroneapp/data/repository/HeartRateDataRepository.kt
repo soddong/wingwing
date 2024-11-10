ede68482e723e5a20f7a07acc22755557a617e6d
HannaKang <hanna0175@gmail.com>
2024-11-11 02:57:32 +0900

    [#S11P31A307-327] Refactor: 심박수 데이터 처리 로직 수정
    - pulseFlag 값을 true -> 1, false -> 0으로 변환하여 heartRateViewModel에 업데이트
    - 로컬 데이터가 없는 경우 로그 추가

--- shieldroneapp/mobile/src/main/java/com/ssafy/shieldroneapp/data/repository/HeartRateDataRepository.kt
+++ shieldroneapp/mobile/src/main/java/com/ssafy/shieldroneapp/data/repository/HeartRateDataRepository.kt
@@ -19,40 +19,36 @@ class HeartRateDataRepository @Inject constructor(
 
     private val coroutineScope = CoroutineScope(Dispatchers.IO + Job())
 
-    // 로컬에서 데이터 주기적으로 가져와 전송하는 메서드
     fun startSendingLocalHeartRateData(intervalMillis: Long = 5000L) {
         coroutineScope.launch {
             while (isActive) {
                 val heartRateData = localDataSource.getHeartRateData() 
                 
                 if (heartRateData != null) {
-                    // 데이터를 웹소켓으로 전송
                     processHeartRateData(heartRateData)
                 } else {
-//                    Log.d(TAG, "로컬에 심박수 데이터가 없습니다.")
+                   Log.d(TAG, "로컬에 심박수 데이터가 없습니다.")
                 }
                 delay(intervalMillis)
             }
         }
     }
     
-    // processHeartRateData에서 웹소켓으로 데이터 전송
     suspend fun processHeartRateData(data: HeartRateData) {
         try {
             webSocketService.sendHeartRateData(data)
-            // 심박수 상태 업데이트
-            heartRateViewModel.updateHeartRate(data.pulseFlag)
+            // Boolean을 Int로 변환 (true -> 1, false -> 0)
+            val pulseValue = if (data.pulseFlag) 1 else 0
+            heartRateViewModel.updateHeartRate(pulseValue)
             Log.d(TAG, "심박수 데이터가 처리되어 서버에 전송되었습니다.")
         } catch (e: Exception) {
             Log.e(TAG, "심박수 데이터 처리 중 오류 발생", e)
-            // 에러가 발생해도 로컬에 저장만 하고 연결은 유지
             localDataSource.saveHeartRateData(data)
         }
     }
 
 
 
-    // 데이터 전송 중지 메서드
     fun stopSendingData() {
         coroutineScope.coroutineContext.cancelChildren()
     }
