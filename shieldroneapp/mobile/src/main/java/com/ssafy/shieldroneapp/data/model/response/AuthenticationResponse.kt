cee860e7a04f2faab218d24f70c6ed3036567bbc
godeeyeon <godeeyeon@gmail.com>
2024-11-12 09:21:21 +0900

    [S11P31A307-303] Refactor: 인증 관련 응답 data class 파일 구조 변경
    - done: 에러 응답 data class 분리 (기존 AuthenticationResponse 파일에서 AuthenticationErrorResponse 제거 후 ErrorResponse 파일 생성하여 이동 및 rename)

--- shieldroneapp/mobile/src/main/java/com/ssafy/shieldroneapp/data/model/response/AuthenticationResponse.kt
+++ shieldroneapp/mobile/src/main/java/com/ssafy/shieldroneapp/data/model/response/AuthenticationResponse.kt
@@ -14,13 +14,3 @@ data class TokenResponse(
     val accessToken: String,
     val refreshToken: String
 )
-
-/**
- * 3. 400번 오류 시 서버로부터 받는 인증 관련 오류 응답 데이터 클래스
- *
- * 번호 인증 및 본인 인증 과정에서 발생할 수 있는 다양한 오류 코드를 포함합니다.
- */
-data class AuthenticationErrorResponse(
-    val code: String,
-    val message: String
-)
