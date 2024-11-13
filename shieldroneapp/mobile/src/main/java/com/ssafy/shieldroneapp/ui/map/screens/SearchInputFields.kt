4f072ae62fe4435481a711a9597986f56fe475d5
godeeyeon <godeeyeon@gmail.com>
2024-11-14 01:11:22 +0900

    [S11P31A307-274] Feat: SearchInputFields - 출발지 검색 입력 후, Enter 키로도 주변 정류장 조회 이벤트 호출할 수 있도록 추가 / Enter 치면 키보드 숨기기(keyboardController 활용) 통해 UX 개선

--- shieldroneapp/mobile/src/main/java/com/ssafy/shieldroneapp/ui/map/screens/SearchInputFields.kt
+++ shieldroneapp/mobile/src/main/java/com/ssafy/shieldroneapp/ui/map/screens/SearchInputFields.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
@@ -11,6 +12,7 @@ import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.ssafy.shieldroneapp.utils.rememberKeyboardController
 
 @Composable
 fun SearchInputFields(
@@ -19,6 +21,9 @@ fun SearchInputFields(
     onStartTextChange: (String) -> Unit,
     onEndTextChange: (String) -> Unit
 ) {
+    // 키보드 매니저 생성 (맵 클릭 시, 키보드 숨기기 위해)
+    val keyboardController = rememberKeyboardController()
+
     Column (
         modifier = Modifier.fillMaxWidth().padding(16.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
@@ -29,7 +34,13 @@ fun SearchInputFields(
             modifier = Modifier.fillMaxWidth(),
             label = { Text("출발지 검색") },
             colors = TextFieldDefaults.textFieldColors(backgroundColor = MaterialTheme.colors.background),
-            singleLine = true  // 한 줄로 설정
+            singleLine = true,  // 한 줄로 설정
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    onStartTextChange(startText)  // Enter 키 누를 때 검색 호출
+                    keyboardController.hideKeyboard()    // Enter 후 키보드 숨기기
+                }
+            )
         )
         TextField(
             value = endText,
