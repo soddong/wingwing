baefc91247143736b54ccaf394fc0f49362b9d74
HannaKang <hanna0175@gmail.com>
2024-11-15 12:30:38 +0900

    [#S11P31A307-390] Feat: 심박수 관련 데이터 전송 모델 추가

--- shieldroneapp/mobile/src/main/java/com/ssafy/shieldroneapp/data/model/HeartRateData.kt
+++ shieldroneapp/mobile/src/main/java/com/ssafy/shieldroneapp/data/model/HeartRateData.kt
@@ -3,5 +3,7 @@ package com.ssafy.shieldroneapp.data.model
 data class HeartRateData(
     val pulseFlag: Boolean,
     val bpm: Double,
-    val timestamp: Long,
+    val timestamp: Long = System.currentTimeMillis(),
+    // 10초 지속 여부
+    val sustained: Boolean = false
 )
