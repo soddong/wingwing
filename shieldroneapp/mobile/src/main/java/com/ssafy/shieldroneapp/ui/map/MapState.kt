0f44b8a9b16d64375e3fb6b12152cea2b35988f3
godeeyeon <godeeyeon@gmail.com>
2024-11-17 11:54:44 +0900

    [S11P31A307-335] MapState에 드론 배정 성공/실패/배정 취소 모달 관련 상태 값, 매칭 결과 및 드론 배정 에러 메시지 상태 값 추가

--- shieldroneapp/mobile/src/main/java/com/ssafy/shieldroneapp/ui/map/MapState.kt
+++ shieldroneapp/mobile/src/main/java/com/ssafy/shieldroneapp/ui/map/MapState.kt
@@ -4,6 +4,7 @@ import com.ssafy.shieldroneapp.data.model.DroneState
 import com.ssafy.shieldroneapp.data.model.LatLng
 import com.ssafy.shieldroneapp.data.model.LocationType
 import com.ssafy.shieldroneapp.data.model.RouteLocation
+import com.ssafy.shieldroneapp.data.model.response.DroneMatchResponse
 
 /**
  * 모달 상태를 관리하는 열거형
@@ -37,10 +38,15 @@ data class MapState(
     val showStartMarkerModal: Boolean = false,  // 출발지 마커 정보
     val showEndMarkerModal: Boolean = false,    // 도착지 마커 정보
     val showDroneMatchResultModal: Boolean = false, // 드론 매칭 결과
+    val showDroneAssignmentSuccessModal: Boolean = false, // 드론 배정 성공
+    val showDroneAssignmentFailureModal: Boolean = false, // 드론 배정 실패
+    val showCancelSuccessModal: Boolean = false, // 드론 배정 취소 완료
 
     // 드론 상태
     val droneState: DroneState? = null,
+    val droneMatchResult: DroneMatchResponse? = null, // 드론 매칭 결과 추가
     val remainingTime: Int = 600, // 드론 배정 후 매칭까지 유효기간 타이머 10분 (초 단위)
+    val droneAssignmentError: String? = null, // 드론 배정 에러
 
     // UI 상태
     val isLoading: Boolean = false,
