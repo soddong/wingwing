1d926f538c1cbd2dc152d647b35f158fd1c36761
godeeyeon <godeeyeon@gmail.com>
2024-11-19 03:15:26 +0900

    [S11P31A307-418] Design: 랜딩 페이지 디자인 - 배경 이미지 및 앱 제목 타이핑 효과(TypingText) 추가, 3초 후 화면 자동 넘김 처리

--- shieldroneapp/mobile/src/main/java/com/ssafy/shieldroneapp/ui/landing/LandingScreen.kt
+++ shieldroneapp/mobile/src/main/java/com/ssafy/shieldroneapp/ui/landing/LandingScreen.kt
@@ -1,59 +1,68 @@
 package com.ssafy.shieldroneapp.ui.landing
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import com.ssafy.shieldroneapp.R
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.ssafy.shieldroneapp.ui.components.ButtonType
-import com.ssafy.shieldroneapp.ui.components.CustomButton
 import com.ssafy.shieldroneapp.ui.components.Layout
-
+import com.ssafy.shieldroneapp.ui.components.TypingText
+import kotlinx.coroutines.delay
 
 @Composable
 fun LandingScreen(
-    onAuthClick: () -> Unit,
+    navigateToNextScreen: () -> Unit,
     viewModel: LandingViewModel = hiltViewModel()
 ) {
+    // 3초 후 자동 화면 전환
+    LaunchedEffect(Unit) {
+        delay(3000L)
+        navigateToNextScreen()
+    }
+
     Layout {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(48.dp)
-                .padding(top = 78.dp, bottom = 16.dp),
-            verticalArrangement = Arrangement.SpaceBetween,
+        Box(
+            modifier = Modifier.fillMaxSize()
         ) {
+            // 배경 이미지
+            Image(
+                painter = painterResource(id = R.drawable.landing_background),
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop // 이미지를 화면 크기에 맞게 조정
+            )
+
             Column(
-                modifier = Modifier.fillMaxWidth(),
-                verticalArrangement = Arrangement.spacedBy(60.dp)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxSize()
+                    .padding(top = 220.dp, bottom = 24.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceBetween
             ) {
-                Text(
-                    text = "Shieldrone",
-                    style = MaterialTheme.typography.h1,
-                    color = MaterialTheme.colors.onBackground
+                // 프로젝트 제목
+                TypingText(
+                    fullText = "WINGWING", // 타이핑 효과 적용
                 )
 
                 Text(
-                    text = "Shieldrone과 함께\n" +
-                            "안전 귀가를 시작하세요.\n" +
-                            "든든한 동행이 \n" +
-                            "언제나 당신의 곁을 지킵니다.",
-                    style = MaterialTheme.typography.h5.copy(lineHeight = 32.sp),
-                    color = MaterialTheme.colors.onBackground
+                    text = "WINGWING과 함께 안전 귀가를 시작하세요.\n" +
+                            "든든한 동행이 언제나 당신의 곁을 지킵니다.",
+                    style = MaterialTheme.typography.body2.copy(lineHeight = 22.sp),
+                    color = MaterialTheme.colors.background,
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center
                 )
             }
-
-            CustomButton(
-                text = "시작하기",
-                onClick = {
-                    viewModel.startAudioService()
-                    onAuthClick()
-                },
-                type = ButtonType.LARGE
-            )
         }
     }
 }
